6bca086e906850d0a27a9f18018314cfbaeab687
tinosg <valentinogsarmiento7@gmail.com>
2025-09-24 14:55:29 -0300

    Add files via upload

---
+++ eventos/index.js
@@ -0,0 +1,11 @@
+// Creamos la función para cerrar sesión
+function cerrarSesion() {
+
+    alert("Has cerrado sesión, gracias por tu visita.")
+}
+
+// Función para salir de la página
+
+function salirPagina() {
+    location.href = "https://www.google.com"; // Redirige a Google
+}
